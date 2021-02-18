19093957783563105fc3f18f7f16031adf12d944
James Wordie <james.wordie@yahoo.com>
2021-02-18 13:19:12 +0000

    seed run

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,21 @@
-require "json"
-require 'open-uri'
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "dry vermouth")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "creme de cacoa")
+Ingredient.create(name: "grenadine")
+Ingredient.create(name: "port")
+Ingredient.create(name: "sloe gin")
+Ingredient.create(name: "cointreau")
 
-#Ingredient.create(name: "lemon")
-#Ingredient.create(name: "ice")
-#Ingredient.create(name: "mint leaves")
-#Ingredient.create(name: "gin")
-#Ingredient.create(name: "dry vermouth")
-#Ingredient.create(name: "tequila")
-#Ingredient.create(name: "lime")
-#Ingredient.create(name: "creme de cacoa")
-#Ingredient.create(name: "grenadine")
-#Ingredient.create(name: "port")
-#Ingredient.create(name: "sloe gin")
-#Ingredient.create(name: "cointreau")
+# require "json"
+# require 'open-uri'
 
-
-10.times do
-  url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-  parsed = JSON.parse(URI.open(url.read))
-  Ingredient.create(name: parsed)
-end
+# 10.times do
+#   url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+#   parsed = JSON.parse(URI.open(url.read))
+#   Ingredient.create(name: parsed)
+# end
