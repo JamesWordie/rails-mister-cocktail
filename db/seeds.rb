8196eba7259b4e3ec16e5f5146125fdc35fa9aa5
James Wordie <james.wordie@yahoo.com>
2021-02-18 13:54:59 +0000

    added seed from api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,23 @@
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "gin")
-Ingredient.create(name: "dry vermouth")
-Ingredient.create(name: "tequila")
-Ingredient.create(name: "lime")
-Ingredient.create(name: "creme de cacoa")
-Ingredient.create(name: "grenadine")
-Ingredient.create(name: "port")
-Ingredient.create(name: "sloe gin")
-Ingredient.create(name: "cointreau")
+require "json"
+require 'open-uri'
 
-# require "json"
-# require 'open-uri'
+Ingredient.destroy_all
 
-# 10.times do
-#   url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-#   parsed = JSON.parse(URI.open(url.read))
-#   Ingredient.create(name: parsed)
-# end
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+parsed = JSON.parse(URI.open(url).read)
+parsed.values.flatten.each do |ingred|
+  Ingredient.create(name: ingred.values.first)
+end
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "gin")
+# Ingredient.create(name: "dry vermouth")
+# Ingredient.create(name: "tequila")
+# Ingredient.create(name: "lime")
+# Ingredient.create(name: "creme de cacoa")
+# Ingredient.create(name: "grenadine")
+# Ingredient.create(name: "port")
+# Ingredient.create(name: "sloe gin")
+# Ingredient.create(name: "cointreau")
