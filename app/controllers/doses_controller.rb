f70710f0131ce0e067d37b428b32a49670f48f09
James Wordie <james.wordie@yahoo.com>
2021-02-18 18:15:32 +0000

    working on style

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,37 +1,38 @@
 class DosesController < ApplicationController
-  before_action :find_cocktail, only: [:new, :create, :destroy]
-  before_action :find_ingredient, only: [:new, :create, :destroy]
+  # before_action :find_cocktail, only: [:new, :create, :destroy]
 
   def new
     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
     @dose = Dose.new(strong_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
-    @dose.ingredient = @ingredient
     if @dose.save
-      redirect_to ????
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
   def destroy
-
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    @cocktail = @dose.cocktail
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
 
   def strong_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
-  def find_cocktail
-    @cocktail = Cocktail.find(params[:cocktail_id])
-  end
+#  def find_cocktail
+#    @cocktail = Cocktail.find(params[:cocktail_id])
+#  end
 
-  def find_ingredient
-    @ingredient = Ingredient.find(params[:ingredient_id])
-  end
+
 end
