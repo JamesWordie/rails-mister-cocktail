7d9acfaec1488b95aac1684b96766f3de675a3a2
James Wordie <james.wordie@yahoo.com>
2021-02-18 13:17:05 +0000

    before seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require 'open-uri'
+
+#Ingredient.create(name: "lemon")
+#Ingredient.create(name: "ice")
+#Ingredient.create(name: "mint leaves")
+#Ingredient.create(name: "gin")
+#Ingredient.create(name: "dry vermouth")
+#Ingredient.create(name: "tequila")
+#Ingredient.create(name: "lime")
+#Ingredient.create(name: "creme de cacoa")
+#Ingredient.create(name: "grenadine")
+#Ingredient.create(name: "port")
+#Ingredient.create(name: "sloe gin")
+#Ingredient.create(name: "cointreau")
+
+
+10.times do
+  url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+  parsed = JSON.parse(URI.open(url.read))
+  Ingredient.create(name: parsed)
+end
