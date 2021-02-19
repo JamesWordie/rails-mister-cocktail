3c6be63d07c69f934a190ed7aa4f88c094c5bac8
James Wordie <james.wordie@yahoo.com>
2021-02-19 15:52:47 +0000

    changes to delete paths

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [:new, :create, :show]
   end
+
   resources :doses, only: [:destroy]
 
   resources :reviews, only: [:destroy]
